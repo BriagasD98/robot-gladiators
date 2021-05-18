65af6869d1774ff7ceb9a17089de9b0acb0fcc5a
BriagasD98 <briagasdavid@yahoo.com>
2021-05-18 18:51:53 -0500

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,17 +3,36 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-//You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//   * Fight all enemy-robots
+//   * Defeat each enemy-robot
+// "LOSE" - PLayer robot's health is zero or less
+
+//You can also log multiple values at once like this
+/*console.log(playerName, playerAttack, playerHealth);
+
+console.log(enemyNames);
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
+console.log(enemyNames.length);
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+} */
+
 // create function (Functon Expression)
-var fight = function() {
+var fight = function(enemyName) {
     //alert players that they are starting the round
     window.alert("Welcome to Robot Galdiators!");
+
+    //ask player if theyd like to fight or run
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
     // if player chooses to fight, then fight
@@ -57,10 +76,13 @@ var fight = function() {
         else {
             fight();
         }
+        //if player did not choose 1 or 2 in prompt
         } else {
             window.alert("You need to choose a valid option. Try again!");
         }
     
 };
 // execute function
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
